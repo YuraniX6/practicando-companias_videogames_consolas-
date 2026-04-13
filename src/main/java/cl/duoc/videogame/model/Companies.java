2933635f19bd99f65c6bfcdd1873b795f2c1c608
YuraniX6 <javi.mendeza@duocuc.cl>
2026-04-13 12:06:15 -0400

    cambio de uno a muchos y de muchos a uno

--- src/main/java/cl/duoc/videogame/model/Companies.java
+++ src/main/java/cl/duoc/videogame/model/Companies.java
@@ -1,7 +1,10 @@
 package cl.duoc.videogame.model;
 
-import jakarta.validation.constraints.NotBlank;
+import java.util.Set;
+import java.util.HashSet;
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
@@ -21,5 +24,8 @@ public class Companies {
     private String name;
 
     private String country;
+    //uno a muchos relacion
+    @OneToMany(mappedBy = "companie")//indica el dueño de la relacion
+    private Set<Console> consoles = new HashSet<>();//incluimos la coleccion a la relaccion
 
 }
--- src/main/java/cl/duoc/videogame/model/Console.java
+++ src/main/java/cl/duoc/videogame/model/Console.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -11,6 +13,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.util.Set;
+import java.util.HashSet;
 
 @Entity
 @Table(name="consoles")
@@ -29,5 +33,9 @@ public class Console {
     private String name;
 
     private Integer releaseYear;
+
+    @ManyToOne
+    @JoinColumn(name = "companie_id", nullable = false)
+    private Companies companie;
 
 }
